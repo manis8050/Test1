16f74b743ccc1030ccde0264dd1d99b42afdf336
manis <manis@DESKTOP-1RBJ5DQ>
2020-05-25 22:27:14 -0400

    Added a new Utility class

---
+++ Test/src/main/java/org/iit/Test/Utility.java
@@ -0,0 +1,24 @@
+package org.iit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.FileHandler;
+
+public class Utility {
+	
+
+
+	public static String getFutureDate(int days,String pattern)
+	{
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, days);
+		Date d=cal.getTime();
+		SimpleDateFormat sdf=new SimpleDateFormat(pattern);
+		String date=sdf.format(d);
+		return date;
+		
+	}
+}
